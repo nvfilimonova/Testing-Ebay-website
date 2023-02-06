cec403d0e1f643170b0c4c46f2db6d7adeb04f51
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-06 14:30:50 +0200

    Update Ebay test.ts

--- Ebay test.ts
+++ Ebay test.ts
@@ -4,3 +4,5 @@ test('Nataly test', async ({page})=>{
   await page.goto('https://www.ebay.co.uk/n/all-categories/')
   await page.locator('.btn btn-prim gh-spr').click
 })
+// Expect a title "to contain" a substring.
+await expect('.btn btn-prim gh-spr').toContain('Search');
