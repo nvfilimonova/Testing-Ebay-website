40cbaece59b096c2b7784f368eeb70d0b8fbad86
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-06 16:17:18 +0200

    Code correctness

--- Ebay test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect, type Page } from '@playwright/test';
-
-test('Nataly test', async ({page})=>{
-  await page.goto('https://www.ebay.co.uk/n/all-categories/')
-  await page.locator('.btn btn-prim gh-spr').click
-})
-// Expect a title "to contain" a substring.
-await expect('.btn btn-prim gh-spr').toContain('Search');
