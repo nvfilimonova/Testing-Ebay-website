8df71759557f3b2a45e644f844aeb222257cd5fa
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-06 16:46:09 +0200

    Update Ebay-test.spec.ts

--- tests/Ebay-test.spec.ts
+++ tests/Ebay-test.spec.ts
@@ -2,6 +2,7 @@ import { test, expect, Locator } from '@playwright/test';
 
 test('Ebay Website Button Test', async ({page})=>{
   const button = 'input[value="Search"]';
+  
   await page.goto('https://www.ebay.co.uk/n/all-categories/')
   await expect(page.locator(button)).toBeVisible();
   await page.locator(button).click
