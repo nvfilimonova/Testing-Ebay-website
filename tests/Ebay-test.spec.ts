cc006a9d01714561ad73cb82c75de87d8fd3b8da
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-06 20:04:47 +0200

    Update Ebay-test.spec.ts

--- tests/Ebay-test.spec.ts
+++ tests/Ebay-test.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect, Locator } from '@playwright/test';
 
-test('Ebay Website Button Test', async ({page})=>{
-  const button = 'input[value="Search"]';
-  
-  await page.goto('https://www.ebay.co.uk/n/all-categories/')
-  await expect(page.locator(button)).toBeVisible();
-  await page.locator(button).click
+test('Google Search Website Button Test', async ({page})=>{
+
+  await page.goto('https://www.google.com/')
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Gmail');
 })
 
 
