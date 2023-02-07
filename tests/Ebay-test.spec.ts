ce184b30832429d16f62e840bfce8758272a894b
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-07 11:23:37 +0200

    var3

--- tests/Ebay-test.spec.ts
+++ tests/Ebay-test.spec.ts
@@ -3,8 +3,7 @@ import { test, expect, Locator } from '@playwright/test';
 test('Google Search Website Button Test', async ({page})=>{
 
   await page.goto('https://www.google.com/')
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Gmail');
+  await expect(page.locator('text=Gmail').first()).toBeVisible();
 })
 
 
