4375985b58a2624c0f1498e55e1c09d7054f9c16
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-07 16:07:39 +0200

    Update ebay-test.spec.ts

--- tests/ebay-test.spec.ts
+++ tests/ebay-test.spec.ts
@@ -7,6 +7,21 @@ test('Ebay Website Button Test', async ({page})=>{
   await expect(page.locator(button)).toBeVisible();
   await page.locator(button).click
 })
+ 
+//=======================
 
+import { test, expect, Locator } from '@playwright/test';
+
+test('Google Search Website Button Test', async ({page})=>{
+  await page.goto('https://www.google.com/')
+  const gbutton = page.getByText('Gmail');
+  await expect(gbutton).toBeVisible();
+
+  await expect(gbutton).toContainText('Gmail');
+
+  await gbutton.click();
+  await expect(gbutton).toBeHidden()
+
+});
 
 
