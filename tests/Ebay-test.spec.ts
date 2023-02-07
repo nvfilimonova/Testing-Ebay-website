af90976604efbca4dd45d230a1ed7fa54187e47a
nvfilimonova <124364705+nvfilimonova@users.noreply.github.com>
2023-02-07 14:06:45 +0200

    Update Ebay-test.spec.ts

--- tests/Ebay-test.spec.ts
+++ tests/Ebay-test.spec.ts
@@ -1,10 +1,21 @@
 import { test, expect, Locator } from '@playwright/test';
 
 test('Google Search Website Button Test', async ({page})=>{
-
   await page.goto('https://www.google.com/')
-  await expect(page.locator('text=Gmail').first()).toBeVisible();
-})
+  const gbutton = page.getByText('Gmail');
+  await expect(gbutton).toBeVisible();
+
+  await expect(gbutton).toContainText('Gmail');
+
+  await gbutton.click();
+  await expect(gbutton).toBeHidden()
+
+
+ //const { getByTestId } = render(<a data-testid='link' href="https://test.com">Click Me</a>);
+  //expect(getByTestId('link')).toHaveAttribute('href', 'https://test.com');
+  
+
+});
 
 
 
